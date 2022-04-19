6fc9148949d284d6d46651f16a90f09e8aba7568
andrewNEW42 <sanjino56@gmail.com>
2022-04-19 20:50:06 +0300

    dockerization

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,28 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entity/users.entity';
+
+const entities = [User];
+
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers:[AppController],
+  providers:[AppService],
+  imports:[
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type:"postgres",
+      host: "172.17.0.2",
+      port: 5432,
+      username: "myusername",
+      password: "mypassword",
+      database: "notes",
+      entities: entities,
+      synchronize: false,
+    })],
 })
 export class AppModule {}
